357776154231482c4a078c30bb5640bfb2bd4951
AgusBarba03 <agustinbarbalase@gmail.com>
2022-03-02 18:02:18 -0300

    Modify vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig, loadEnv } from "vite";
-import { resolve } from "path";
+import { join } from "path";
 import react from "@vitejs/plugin-react";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, join(__dirname, '../'));
   console.log(env.VITE_BASE_URL);
   return {
     plugins: [
