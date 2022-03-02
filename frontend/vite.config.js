03a5ba1c2fc5f9d1e9afe4b3303cde2d671293c5
AgusBarba03 <agustinbarbalase@gmail.com>
2022-03-02 18:16:33 -0300

    Modify vite.config.js and index.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig, loadEnv } from "vite";
-import { join } from "path";
 import react from "@vitejs/plugin-react";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, join(__dirname, '../'));
+  const env = loadEnv(mode, process.cwd());
   console.log(env.VITE_BASE_URL);
   return {
     plugins: [
@@ -17,7 +16,7 @@ export default defineConfig(({ mode }) => {
         entry: "src/main.jsx",
         inject: {
           data: {
-            baseUrl: env.VITE_BASE_URL,
+            baseUrl: env.VITE_BASE_URL || "http://localhost:3000",
           },
         },
       }),
